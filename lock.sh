c24576e30599da6c3cf6a517dc87986b1d7b30dd
tcelestin-profideo <54313790+tcelestin-profideo@users.noreply.github.com>
2020-03-09 14:25:18 +0100

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,24 +1,80 @@
 #!/bin/sh
 
-icon="$HOME/.config/i3lock/nope.png"
+icon="$HOME/.config/i3lock/other-nope.png"
+emoji_file="$HOME/Documents/emoji_urls.txt"
 img="/tmp/i3lock.png"
+emoji="/tmp/emoji.png"
+wallpapers="$HOME/Images/wallhallapapers/links.txt"
+parameter="$1"
 
-cp $icon /tmp/nope.png
+pixelate () {
+    ## Take a screenshot
+    scrot "$img"
 
-tmp_icon="/tmp/nope.png"
-
-scrot $img
-# Pixelate image
-convert $img -scale 10% -scale 1000% $img
-convert $tmp_icon -scale 25% $tmp_icon
-# Blur image
-#convert $img -blur 0x4 500% $img
-convert $img $tmp_icon -gravity center -composite $img
-
-revert() {
-    xset dpms 0 0 0
+    ## Pixelate screenshot
+    convert $img -scale 20% -scale 500% $img
 }
 
-trap revert HUP INT TERM
+blur () {
+    ## Blur screenshot
+    convert $img -blur 0x2 500% $img
+}
 
-i3lock -i $img
+add_emoji () {
+    ## Download an image from a list of urls
+    wget $(shuf -n 1 $emoji_file) -O $emoji
+    ## Resize the downloaded image
+    convert $emoji -resize 65x65 $emoji
+    ## Insert the image inside the screenshot
+    convert $img $emoji -geometry +960+540 -composite $img
+}
+
+wallpaper () {
+    ## Use wallpaper as image
+    convert $(curl $(shuf -n 1 $wallpapers)) $img
+    #convert $img -resize 1920x1080 $img
+}
+
+joke () {
+    i3lock -u -p win -i $HOME/Images/joke.png
+}
+
+if [ -z $parameter ]
+then
+    echo "HELP:
+    -p  #Pixelate
+    -pe #Pixelate with emoji in the middle
+    -b  #Blur
+    -j  #Fake unlocked screen
+    -w  #Wallpaper"
+    exit
+fi
+
+if [ $parameter = "-b" ]
+then
+    blur
+fi
+
+if [ $parameter = "-p" ]
+then
+    pixelate
+fi
+
+if [ $parameter = "-pe" ]
+then
+    pixelate
+    add_emoji
+fi
+
+if [ $parameter = "-w" ]
+then
+    wallpaper
+fi
+
+if [ $parameter = "-j" ]
+then
+    joke
+fi
+
+## Lock the screen
+i3lock -ui $img
